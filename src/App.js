024fa073ae72e1f6ac6cc7df24e864b2410ac7d9
Bryon Smith <bryon@smithauto.us>
2021-11-04 16:48:19 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useCallback } from 'react'
+import useBookSearch from './useBookSearch'
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+
+  function handleQuery(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+  const { loading, books, error, hasMore } = useBookSearch(query, pageNumber)
+
+  const observer = useRef()
+  const lastBookElement = useCallback(
+    (node) => {
+      if (loading) return
+      if (observer.current) observer.current.disconnect()
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1)
+        }
+      })
+      if (node) observer.current.observe(node)
+    },
+    [loading, hasMore]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <input type="text" value={query} onChange={handleQuery} />
+      <div>
+        {books.map((book, index) => {
+          if (books.length === index + 1) {
+            return (
+              <div key={book} ref={lastBookElement}>
+                {book}
+              </div>
+            )
+          } else {
+            return <div key={book}>{book}</div>
+          }
+        })}
+      </div>
+      <div>{loading && query && `...Loading`}</div>
+      <div>{error && `sorry there was an axios error`}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
